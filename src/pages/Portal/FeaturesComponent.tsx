0e4379bfe19568fee46da6c248b8957785351154
Olivier Scherrer <pode.fr@gmail.com>
2024-06-10 11:30:49 +0200

    Properly load features images and remove S.A.S from the page title

--- src/pages/Portal/FeaturesComponent.tsx
+++ src/pages/Portal/FeaturesComponent.tsx
@@ -1,5 +1,15 @@
 import { PropsWithChildren } from "react";
 
+import a247 from "../../assets/247.png";
+import api from "../../assets/api.png";
+import fx from "../../assets/fx.png";
+import lowFees from "../../assets/low-fees.png";
+import regulated from "../../assets/regulated.png";
+import secured from "../../assets/secured.png";
+import virtualaccount from "../../assets/virtualaccount.png";
+import block from "../../assets/block.png";
+
+
 import "./FeaturesComponents.scss";
 
 interface FeaturesComponentProps {
@@ -11,14 +21,14 @@ export default function FeaturesComponent({ nav }: FeaturesComponentProps) {
     <div className="features-component page" id={nav}>
       <h3>Key features</h3>
       <dl>
-        <Feature asset="api">FX Infrastructure as a Service embed as an API or SDK</Feature>
-        <Feature asset="low-fees">Low & Transparent Fees. Enjoy lower costs compared to traditional methods</Feature>
-        <Feature asset="247">Instant Trading. Immediate execution of trades for optimal efficiency</Feature>
-        <Feature asset="regulated">Fully Regulated. All funds are 100% KYCed. Know your counterparty</Feature>
-        <Feature asset="FX">EUR - USD - GBP</Feature>
-        <Feature asset="secured">Security. Low slippage. No impermanent loss</Feature>
-        <Feature asset="virtualaccount">Virtual Accounts</Feature>
-        <Feature asset="block">Large Liquidity Pool. Ensuring seamless transactions without delays</Feature>
+        <Feature asset={api}>FX Infrastructure as a Service embed as an API or SDK</Feature>
+        <Feature asset={lowFees}>Low & Transparent Fees. Enjoy lower costs compared to traditional methods</Feature>
+        <Feature asset={a247}>Instant Trading. Immediate execution of trades for optimal efficiency</Feature>
+        <Feature asset={regulated}>Fully Regulated. All funds are 100% KYCed. Know your counterparty</Feature>
+        <Feature asset={fx}>EUR - USD - GBP</Feature>
+        <Feature asset={secured}>Security. Low slippage. No impermanent loss</Feature>
+        <Feature asset={virtualaccount}>Virtual Accounts</Feature>
+        <Feature asset={block}>Large Liquidity Pool. Ensuring seamless transactions without delays</Feature>
       </dl>
     </div>
   );
@@ -32,7 +42,7 @@ function Feature({ asset, children }: PropsWithChildren<FeatureProp>) {
   return (
     <div>
       <dt>
-        <img src={`src/assets/${asset}.png`} />
+        <img src={asset} />
       </dt>
       <dd>{children}</dd>
     </div>
