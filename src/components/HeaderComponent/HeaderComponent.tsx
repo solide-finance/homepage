efa9c7a87afc54a5a76fa98d9b17f9698c145d14
Olivier Scherrer <pode.fr@gmail.com>
2024-06-22 15:39:03 +0200

    Add responsive header with expandable menu

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -20,37 +20,68 @@ export default function HeaderComponent() {
   }, []);
 
   return (
-    <header className={cx("header-component", { "is-scrolled": hasScrolled })}>
-      <h1>
-        <a href="/#top">
-          <img src={sflogo} alt="logo" />
-          SolideFinance
-        </a>
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="/#mission">Mission</a>
-          </li>
-          <li>
-            <a href="/#product">Product</a>
-          </li>
-          <li>
-            <a href="/#vision">Vision</a>
-          </li>
-          {/* <li>
+    <>
+      <header className={cx("header-component desktop", { "is-scrolled": hasScrolled })}>
+        <h1>
+          <a href="/#top">
+            <img src={sflogo} alt="logo" />
+            SolideFinance
+          </a>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="/#mission">Mission</a>
+            </li>
+            <li>
+              <a href="/#product">Product</a>
+            </li>
+            <li>
+              <a href="/#vision">Vision</a>
+            </li>
+            {/* <li>
             <a href="#about">About</a>
           </li> */}
-        </ul>
-      </nav>
-      <div className="actions">
-        <a href="mailto:hello@solide.fi" className="button-like" style={{"fontSize": "2em"}}>
-        &#9743;
-        </a>
-        <a href="mailto:hello@solide.fi" className="button-like">
-          Login <span>-&gt;</span>
-        </a>
-      </div>
-    </header>
+          </ul>
+        </nav>
+        <div className="actions">
+          <a href="mailto:hello@solide.fi" className="button-like" style={{ fontSize: "2em" }}>
+            &#9743;
+          </a>
+          <a href="mailto:hello@solide.fi" className="button-like">
+            Login <span>-&gt;</span>
+          </a>
+        </div>
+      </header>
+      <header className={cx("header-component mobile", { "is-scrolled": hasScrolled })}>
+        <details name="menu">
+          <summary>
+            <h1>
+              <a href="/#top">
+                <img src={sflogo} alt="logo" />
+                SolideFinance
+              </a>
+            </h1>
+            <span className="expanded-indicator">&nbsp;</span>
+          </summary>
+          <nav>
+            <ul>
+              <li>
+                <a href="/#mission">Mission</a>
+              </li>
+              <li>
+                <a href="/#product">Product</a>
+              </li>
+              <li>
+                <a href="/#vision">Vision</a>
+              </li>
+              {/* <li>
+            <a href="#about">About</a>
+          </li> */}
+            </ul>
+          </nav>
+        </details>
+      </header>
+    </>
   );
 }
